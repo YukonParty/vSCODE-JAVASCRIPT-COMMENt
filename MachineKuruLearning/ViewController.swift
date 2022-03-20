d645d6fe4bd57d2ceb195778a4a39fc5c4f1d0e1
YukonParty <114738112+YukonParty@users.noreply.github.com>
2022-03-20 19:25:52 +0000

    NFS: Fix a problem with the legacy binary mount code

--- MachineKuruLearning/ViewController.swift
+++ MachineKuruLearning/ViewController.swift
@@ -13,4 +13,4 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup a
+        // Do any additional setup afte
