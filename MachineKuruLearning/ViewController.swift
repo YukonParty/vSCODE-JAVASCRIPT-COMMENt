94e4e4457f9120535ce1dd809b8a10fb551519f4
YukonParty <114738112+YukonParty@users.noreply.github.com>
2022-03-26 10:47:26 +0000

    fix #19

--- MachineKuruLearning/ViewController.swift
+++ MachineKuruLearning/ViewController.swift
@@ -13,4 +13,4 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after lo
+        // Do any additional setup after loading
