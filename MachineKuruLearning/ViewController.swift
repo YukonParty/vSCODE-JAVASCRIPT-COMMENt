1182d04e74841a4aaded5a3abf54495372104eab
YukonParty <114738112+YukonParty@users.noreply.github.com>
2022-05-03 10:43:15 +0000

    Changed directory structure

--- MachineKuruLearning/ViewController.swift
+++ MachineKuruLearning/ViewController.swift
@@ -18,4 +18,4 @@ class ViewController: NSViewController {
 
     override var representedObject: Any? {
         didSet {
-        // Update the view, if alre
+        // Update the view, if already 
